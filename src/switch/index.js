c4af9822433d412d790e259b58e3e2a17971a6fc
东杰 <dongjie.wang@tempoon.com>
2019-05-05 21:46:01 +0800

    add switch

---
+++ src/switch/index.js
@@ -0,0 +1,16 @@
+Component({
+  properties: {
+    color: {
+      type: String,
+      value: ''
+    },
+    checked: {
+      type: Boolean,
+      value: false
+    },
+    disabled: {
+      type: Boolean,
+      value: false
+    }
+  }
+});
